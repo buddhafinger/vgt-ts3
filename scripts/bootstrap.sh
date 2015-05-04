84cb67560e529997ea40156665a1a4747b3552a7
buddhafinger <garry.stevens99@gmail.com>
2015-05-04 15:04:09 +0100

    added more/better idempotence for swap creation, tweaked bootstrap.sh to use new vars and added a service start line

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,5 +2,5 @@
 apt-get install -y python-software-properties
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
-sudo apt-get install -y ansible
+sudo apt-get install -y ansible git pastebinit
 sudo ansible-playbook -i /vagrant/ansible/hosts/ansible_hosts -c "local" /vagrant/ansible/playbooks/ansi-ts3.yml
